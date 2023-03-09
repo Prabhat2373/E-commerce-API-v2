b34c844fbae1f4c09dac808ce14b8bf97df81dee
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2023-03-09 14:56:59 +0530

    auth changes and cart route added

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,9 +9,7 @@ const sendToken = (user, statusCode, res, opt) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    SameSite: 'lax',
     secure: false,
-    sameSite: 'lax'
   };
   if (process.env.NODE_ENV) {
     options.secure = true
