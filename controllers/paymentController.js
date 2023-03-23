2df18a4a4331a5c64ca5b0e5f07fe51c2a52d84c
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2023-03-23 14:23:52 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,14 +3,13 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-  const myPayment = await stripe.paymentIntents.create({
+  const myPayment = await stripe.checkout.sessions.create({
     amount: req.body.amount,
     currency: "inr",
     metadata: {
       company: "wixy-shop",
     },
   });
-
   res
     .status(200)
     .json({ success: true, client_secret: myPayment.client_secret });
